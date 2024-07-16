b408543ea8ab84c66d15b6ad1dbe585de37ce9d8
verseler <handuman.verselerkerr@gmail.com>
2024-07-16 21:04:27 +0800

    feat: add theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,7 +1,25 @@
 import { createTheme } from "@mantine/core";
 
+const primary = [
+  "#ffedea",
+  "#fbdad6",
+  "#eeb4af",
+  "#e08c83",
+  "#d66a5f",
+  "#d05447",
+  "#ce483a",
+  "#b7392d",
+  "#a33126",
+  "#90261e"
+];
+
+const secondary = ["#fbfaf8"];
+
 const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    primary,
+    secondary,
+  },
 });
 
 export default theme;
